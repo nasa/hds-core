4180ff5304e25273886753a7cdd0ec3e175f9386
Abby Bowman <abigail.r.bowman@nasa.gov>
2026-05-17 21:06:06 -0400

    ci: install remark-lint with consistent + recommended presets

---
+++ .remarkrc.mjs
@@ -0,0 +1,11 @@
+const config = {
+  plugins: [
+    'remark-frontmatter',
+    'remark-gfm',
+    'remark-validate-links',
+    'remark-preset-lint-recommended',
+    'remark-preset-lint-consistent',
+  ],
+};
+
+export default config;
